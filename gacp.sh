de76af30d7573af970a765c130d7535b1c928a93
user1234 <user1234@user1234s-MacBook.local>
2025-12-06 06:50:27 +0100

    'add gacp'

--- gacp.sh
+++ gacp.sh
@@ -3,14 +3,22 @@
 function addcommitpush () {
 
 current=$(git branch | grep "*" | cut -b 3-)
-
-echo $current
-
 message=\'"$@"\'
 
-echo $message
+clear
+echo -------------------------------------------------
+echo -------------------------------------------------
+echo Commit to branch: $current with message: $message
+
+read -p "Do you want to proceed? (y/n) " yn
+case $yn in
+    [Yy]* ) echo "Proceeding...";;
+    [Nn]* ) echo "Exiting..."; exit;;
+    * ) echo "Please answer yes or no.";;
+esac
 
 git add -A && git commit -a -m "$message"
+git push origin main
 
 # echo "Where to push?"
 # read -i "$current" -e branch
