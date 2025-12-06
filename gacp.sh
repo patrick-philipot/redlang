b66762e04ea5c79db4df3ea2fcd786439a77de5d
user1234 <user1234@user1234s-MacBook.local>
2025-12-06 06:41:29 +0100

    'add gacp'

--- gacp.sh
+++ gacp.sh
@@ -4,20 +4,25 @@ function addcommitpush () {
 
 current=$(git branch | grep "*" | cut -b 3-)
 
+echo $current
+
 message=\'"$@"\'
+
+echo $message
+
 git add -A && git commit -a -m "$message"
 
-echo "Where to push?"
-read -i "$current" -e branch
+# echo "Where to push?"
+# read -i "$current" -e branch
 
-echo "You sure you wanna push? (y/n)"
-read -i "y" -e yn
+# echo "You sure you wanna push? (y/n)"
+# read -i "y" -e yn
 
-if [ "$yn" = y ]; then
-  git push origin "$branch"
-else
-  echo "Have a nice day!"
-fi
+# if [ "$yn" = y ]; then
+#   git push origin "$branch"
+# else
+#   echo "Have a nice day!"
+# fi
 
 }
 
